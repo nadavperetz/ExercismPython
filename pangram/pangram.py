4b1a44fd6d31c9a3f5358cc5a04d399714bc8def
nadavperetz <nadavperetz@gmail.com>
2017-04-11 12:50:47 -0300

    submit ex 4

--- pangram/pangram.py
+++ pangram/pangram.py
@@ -1,2 +1,15 @@
-def is_pangram():
-    pass
+from string import ascii_lowercase
+
+
+def is_pangram(word):
+    # Creating alphabet list
+    full_list = [letter for letter in ascii_lowercase]
+    for letter in word:
+        letter = letter.lower()
+        if letter in full_list:
+            full_list.remove(letter)
+    # Python empty list is equivalent to a boolean False, True otherwise
+    return not full_list
+
+
+
