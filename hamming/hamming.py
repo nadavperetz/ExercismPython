58d64b17edb77f1b3541e6b90f27a23fcd997b14
nadavperetz <nadavperetz@gmail.com>
2017-04-11 13:05:18 -0300

    Submit Ex 6

--- hamming/hamming.py
+++ hamming/hamming.py
@@ -1,2 +1,14 @@
-def distance():
-    pass
+
+def distance(dna_strand_one, dna_strand_two):
+    # Check if both have same size
+    if len(dna_strand_one) != len(dna_strand_two):
+        raise ValueError
+
+    distance_count = 0
+
+    for nucleotide_one, nucleotide_two in zip(dna_strand_one, dna_strand_two):
+        if not nucleotide_one == nucleotide_two:
+            distance_count += 1
+
+    return distance_count
+
