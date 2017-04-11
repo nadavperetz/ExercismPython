ddd62fc3c0e170cf96db37750ec8d64c775db9bb
nadavperetz <nadavperetz@gmail.com>
2017-04-11 12:04:41 -0300

    submit ex 3

--- isogram/isogram.py
+++ isogram/isogram.py
@@ -1,2 +1,14 @@
-def is_isogram():
-    pass
+
+def is_isogram(word):
+    used_letters = []
+    for letter in word:
+        # ignore non letter character
+        if letter.isalpha():
+            # cast all letter to lower case
+            letter = letter.lower()
+            if letter not in used_letters:
+                used_letters.append(letter)
+            else:
+                return False
+    return True
+
