6061b527dbd16dab045d06d666171d65a7f95360
nadavperetz <nadavperetz@gmail.com>
2017-04-11 12:54:59 -0300

    Submit Ex 5

--- rna-transcription/rna_transcription.py
+++ rna-transcription/rna_transcription.py
@@ -1,2 +1,12 @@
-def to_rna():
-    pass
+rna_transcription_dict = {"G": "C", "C": "G", "T": "A", "A": "U"}
+
+
+def to_rna(dna_strand):
+    rna_strand = ""
+    for nucleotide in dna_strand:
+        try:
+            rna_strand += rna_transcription_dict[nucleotide]
+        except KeyError:
+            return ''
+    return rna_strand
+
